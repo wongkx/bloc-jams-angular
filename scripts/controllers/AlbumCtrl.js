72c7a72597e962e7260ce73007360432761120da
Wong <wongk13@medtronic.com>
2017-04-08 15:22:13 -0500

    Checkpoint 6 Services part 1

--- scripts/controllers/AlbumCtrl.js
+++ scripts/controllers/AlbumCtrl.js
@@ -1,6 +1,6 @@
 (function() {
-    function AlbumCtrl(){
-        this.albumData = angular.copy(albumPicasso);        
+    function AlbumCtrl(Fixtures){
+        this.albumData = Fixtures.getAlbum();        
         //this.songs = [];
         this.songs = angular.copy(this.albumData.songs);
         for (var i = 0; i< this.albumData.songs.length; i++){
@@ -12,5 +12,5 @@
     
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', AlbumCtrl);
+        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
