eff40a5801df44f7aea328317bfba761e2be6240
Wong <wongk13@medtronic.com>
2017-04-09 22:45:01 -0500

    Checkpoint 7 Services Part2

--- scripts/services/Fixtures.js
+++ scripts/services/Fixtures.js
@@ -9,11 +9,11 @@
             year: '1881',
             albumArtUrl: '/assets/images/album_covers/01.png',
             songs: [
-                { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
-                { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
-                { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
-                { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
-                { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' } 
+                { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue.mp3' },
+                { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green.mp3' },
+                { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red.mp3' },
+                { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink.mp3' },
+                { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta.mp3' } 
             ]
         };
 
