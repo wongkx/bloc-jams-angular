1960449814d399e6513edceb861308d2404a7fe0
Wong <wongk13@medtronic.com>
2017-04-10 23:59:49 -0500

    Checkpoint 8 Services Part3

--- scripts/services/SongPlayer.js
+++ scripts/services/SongPlayer.js
@@ -1,22 +1,28 @@
 (function() {
-    function SongPlayer() {
- /**
+    function SongPlayer(Fixtures) {
+
+        /******
+PRIVATE
+******/
+
+        /**
  * @desc SongPlayer object
  * @type {Object}
  */
         var SongPlayer = {};
- /**
- * @desc song object
+        /**
+ * @desc Album object
  * @type {Object}
  */
-        var currentSong= null;     
- /**
+        var currentAlbum = Fixtures.getAlbum();
+
+        /**
  * @desc Buzz object audio file
  * @type {Object}
  */
         var currentBuzzObject = null;
 
-/**
+        /**
  * @function setSong
  * @desc Stops currently playing song and loads new audio file as currentBuzzObject
  * @param {Object} song
@@ -24,15 +30,15 @@
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
-                currentSong.playing = null;
+                SongPlayer.currentSong.playing = null;
             }
             currentBuzzObject = new buzz.sound(song.audioUrl, {
                 format: ['mp3'],
                 preload: true
             });
-            currentSong = song;
+            SongPlayer.currentSong = song;
         }
-/**
+        /**
  * @function playSong
  * @desc Plays selected song and set playing as True
  * @param {Object} song
@@ -41,30 +47,73 @@
             currentBuzzObject.play();
             song.playing = true;
         }
-/**
+        /**
+ * @function getSongIndex
+ * @desc Returns index of the song
+ * @param {Object} song
+ */
+        var getSongIndex = function(song) {
+            return currentAlbum.songs.indexOf(song);
+        };
+
+        /******
+PUBLIC
+******/
+
+        /**
+ * @desc song object
+ * @type {Object}
+ */
+        SongPlayer.currentSong = null;
+        /**
  * @function play
- * @desc Plays song if clicked on a new song, otherwise pauses it
+ * @desc Plays song if clicked on a new song or a paused song
  * @param {Object} song
  */
         SongPlayer.play = function(song) {
-            if (currentSong !== song) {
+            song = song || SongPlayer.currentSong;
+            if (SongPlayer.currentSong !== song) {
                 setSong(song);
                 playSong(song);
             }
-            else if (currentSong === song) {
+            else if (SongPlayer.currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     playSong(song);
                 }
             }
         };
+/**
+ * @function pause
+ * @desc Pauses song if clicked on a currently playing song 
+ * @param {Object} song
+ */
         SongPlayer.pause = function(song) {
+            song = song || SongPlayer.currentSong;
             currentBuzzObject.pause();
             song.playing = false;
+        };
+        
+/**
+ * @function previous
+ * @desc Goes to previous song, decrement the song index 
+ */
+        SongPlayer.previous = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--;
+
+            if (currentSongIndex < 0) {
+                currentBuzzObject.stop();
+                SongPlayer.currentSong.playing = null;
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
         };
         return SongPlayer;
     }
 
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);
+        .factory('SongPlayer', ['Fixtures', SongPlayer]);
 })();
